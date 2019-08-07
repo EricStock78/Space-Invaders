eaee4d566abeddfc2d8e55a3a5f8f783cc02b0c9
gabrielB.walsh <g.b.j.walsh@gmail.com>
2019-08-07 17:52:29 -0230

    added a moose class

---
+++ src/main/java/actors/Moose.java
@@ -0,0 +1,27 @@
+package actors;
+
+import game.Stage;
+
+/**
+ * this is a copy paste of the tnt class
+ */
+public class Moose extends Actor {
+    public Moose(Stage canvas) {
+        super(canvas, 80, 80, 80, 80);
+
+        vx = -1;
+
+        sprites = new String[] {"tnt.png"};
+
+        posX = 500;
+    }
+
+
+    public void update() {
+        super.update();
+        posX += vx;
+        posY += vy;
+    }
+
+
+}
