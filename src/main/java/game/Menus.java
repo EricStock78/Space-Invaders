53810f6bb716b4eaabe58d0577dd5fc5dcb0af47
etroke <emmanatalietroke@gmail.com>
2019-08-07 22:04:06 -0230

    workin on them menus

---
+++ src/main/java/game/Menus.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.awt.event.KeyListener;
+
+public class Menus extends Stage implements KeyListener {
+    private static final long serialVersionUID = 1L;
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 600;
+
+    public Menus() {
+
+    }
+
+    public paintStartMenu() {
+
+    }
+
+    public paintOptionsMenu() {
+
+    }
+
+    public
+}
