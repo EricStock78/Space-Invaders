4d03307dc91ea2d505a9c3d7fd9d43f65112b885
Greg Tracey <gtrace24@gmail.com>
2019-08-08 10:23:53 -0230

    Changes to timbit and coffee power-ups

--- src/main/java/actors/Timbit.java
+++ src/main/java/actors/Timbit.java
@@ -2,23 +2,24 @@ package actors;
 
 import game.Stage;
 
+import java.util.Random;
+
 public class Timbit extends Actor{
+    private Random randy = new Random();
     public Timbit(Stage canvas) {
         super(canvas, 80, 80, 80, 80);
 
-        vx = -1;
+        vx = -2;
 
         sprites = new String[] {"timbit.png"};
 
-        posX = 800;
+        posX = 1000;
+        posY = randy.nextInt(600);
     }
-
 
     public void update() {
         super.update();
         posX += vx;
         posY += vy;
     }
-
-
 }
